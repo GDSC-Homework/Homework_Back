43b8aaf6cf2119a29c897c82f309c321927b94da
HyeokJoon Kong <orijoon98@gmail.com>
2022-01-28 17:28:36 +0900

    Feat: Google Login 구현중

---
+++ src/main/java/joon/homework/dto/google/UserInfoDto.java
@@ -0,0 +1,21 @@
+package joon.homework.dto.google;
+
+import lombok.Getter;
+
+@Getter
+public class UserInfoDto {
+
+    private String iss;
+    private String sub;
+    private String azp;
+    private String aud;
+    private String iat;
+    private String exp;
+    private String email;
+    private String email_verified;
+    private String name;
+    private String picture;
+    private String given_name;
+    private String family_name;
+    private String locale;
+}
---
+++ src/main/java/joon/homework/service/GoogleService.java
@@ -0,0 +1,49 @@
+package joon.homework.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import joon.homework.dto.google.UserInfoDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class GoogleService {
+
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+    public UserInfoDto getUserInfoByIdToken(String idToken) {
+        final String tokenInfoUri = "https://oauth2.googleapis.com/tokeninfo";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("id_token", idToken);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(tokenInfoUri, request, String.class);
+
+            UserInfoDto userInfoDto = objectMapper.readValue(response.getBody(), UserInfoDto.class);
+
+            return userInfoDto;
+        } catch (RestClientException | JsonProcessingException e) {
+            log.error(e.getMessage());
+
+            throw new RuntimeException();
+        }
+    }
+}
